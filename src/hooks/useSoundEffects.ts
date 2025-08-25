6ae004548957110cf0d62646aa0679c89530491f
Utkarsh Tiwari <utkarshweb2023@gmail.com>
2025-08-25 14:08:37 +0530

    feat: Increase Sound Effects Volume

--- src/hooks/useSoundEffects.ts
+++ src/hooks/useSoundEffects.ts
@@ -1,11 +1,11 @@
 import useSound from "use-sound";
 
 export const useSoundEffects = () => {
-  const [playMove] = useSound("/sounds/move.mp3", { volume: 0.4 });
-  const [playCheck] = useSound("/sounds/check.mp3", { volume: 0.5 });
-  const [playGameEnd] = useSound("/sounds/end.webm", { volume: 0.6 });
-  const [playPhaseChange] = useSound("/sounds/phase.mp3", { volume: 0.4 });
-  const [playGameStart] = useSound("/sounds/start.mp3", { volume: 0.5 });
+  const [playMove] = useSound("/sounds/move.mp3", { volume: 0.75 });
+  const [playCheck] = useSound("/sounds/check.mp3", { volume: 0.9 });
+  const [playGameEnd] = useSound("/sounds/end.webm", { volume: 1 });
+  const [playPhaseChange] = useSound("/sounds/phase.mp3", { volume: 0.7 });
+  const [playGameStart] = useSound("/sounds/start.mp3", { volume: 0.8 });
 
   return {
     playMove,
